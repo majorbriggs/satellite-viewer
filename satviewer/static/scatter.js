134a26472bb4813234a56556f4a2a7914d2d381a
piotrek <pbaranowski1@gmail.com>
2017-07-17 03:37:25 +0200

    add howto, nicer error message

--- satviewer/static/scatter.js
+++ satviewer/static/scatter.js
@@ -1,16 +1,20 @@
 // Get the modal
 var modal = document.getElementById('myModal');
+var modalInfo = document.getElementById('infoModal');
+var modalError = document.getElementById('errorModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("btn-show-ts-vi");
 
+
+var btnInfo = document.getElementById("btn-ts-vi-info");
+
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   if (tsviSelection == null){
-      alert("Select area for TSVI scatterplot");
+      showError("Select area for TSVI scatterplot");
   }
   else{
     $('#map').hide();
@@ -20,21 +24,35 @@ btn.onclick = function() {
 
 }
 
+
+function showError(){
+    modalError.style.display = "block";
+    $('#map').hide();
+}
+
+
+btnInfo.onclick = function (){
+  $('#map').hide();
+    modalInfo.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+$(document).on('click', '.close', function() {
     modal.style.display = "none";
+    modalInfo.style.display = "none";
       $('#map').show();
 
 }
-
+);
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal || event.target == modalInfo || event.target == modalError) {
+        event.target.style.display = "none";
         $('#map').show();
 
     }
 }
+
 function loadChart(){
 
 if (dataJson != ""){

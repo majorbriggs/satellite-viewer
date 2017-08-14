8a15edf4ceff61607a9b782a63bd64dcea512555
piotrek <pbaranowski1@gmail.com>
2017-08-14 02:17:12 +0200

    update storage path for production

--- satviewer/const.py
+++ satviewer/const.py
@@ -4,9 +4,9 @@ LANDSAT = 'landsat'
 SENTINEL = 'sentinel'
 OUTPUT_BUCKET = 'satellite-viewer-processed'
 WORKSPACE = "sat-viewer"
-# GEOSERVER_STORAGE = 'file:///home/ubuntu/sat-images/'
+PRODUCTION_GEOSERVER_STORAGE = 'file:///home/ubuntu/sat-images/'
 
-GEOSERVER_STORAGE = 'file:///home/piotrek/mgr/datasets/'
+GEOSERVER_STORAGE = PRODUCTION_GEOSERVER_STORAGE #'file:///home/piotrek/mgr/datasets/'
 
 
 

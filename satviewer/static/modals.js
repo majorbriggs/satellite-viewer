3ef83e641fa381681f44ee9856bdb2d61e46be4a
majorbriggs <pbaranowski1@gmail.com>
2017-09-23 13:08:01 +0200

    modals are not used currently

--- satviewer/static/modals.js
+++ satviewer/static/modals.js
@@ -1,20 +1,20 @@
-var modalInfo = document.getElementById('infoModal');
-var btnInfo = document.getElementById("btn-ts-vi-info");
+// var modalInfo = document.getElementById('infoModal');
+// var btnInfo = document.getElementById("btn-ts-vi-info");
 
 
-btnInfo.onclick = function (){
-    modalInfo.style.display = "block";
-}
+// btnInfo.onclick = function (){
+//     modalInfo.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-$(document).on('click', '.close', function() {
-    modalInfo.style.display = "none";
-}
-);
+// // When the user clicks on <span> (x), close the modal
+// $(document).on('click', '.close', function() {
+//     modalInfo.style.display = "none";
+// }
+// );
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modalInfo) {
-        event.target.style.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modalInfo) {
+//         event.target.style.display = "none";
+//     }
+// }
